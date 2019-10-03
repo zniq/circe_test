d998204e76905135e5f07b91459c15c39fd208d3
Osamu Sato <zniq1129@gmail.com>
2019-10-04 01:33:06 +0900

    encode and decode test

--- src/main/scala/circe_test/Main.scala
+++ src/main/scala/circe_test/Main.scala
@@ -2,7 +2,11 @@ package circe_test
 
 import java.time.LocalDateTime
 
+import circe_test.Main.{employee, user}
+import io.circe.syntax._
+import io.circe.parser._
 import circe_test.model.{Address, Employee, User, WrapperClass}
+import io.circe.{Decoder, Encoder}
 
 object Main extends App {
 
@@ -11,16 +15,36 @@ object Main extends App {
   val employee = Employee("duncan", "dev")
 
   val sec = LocalDateTime.now.getSecond
-  val wrapperClass = if (sec % 2 == 0) {
+  if (sec % 2 == 0) {
     println("*** snake_case format.")
-    new WrapperClass("hoge", "fuga", user, employee)
+
+    printEncode(user)
+    printEncodeUsingWrapClass(user, employee)
+    printDecode(user.asJson.noSpaces)
   } else {
     println("*** camelCase format.")
     implicit val ue = User.camelEncoder
     implicit val ee = Employee.camelEncoder
-    new WrapperClass("hoge", "fuga", user, employee)
+    implicit val ud = User.camelDecoder
+
+    printEncode(user)
+    printEncodeUsingWrapClass(user, employee)
+    printDecode(user.asJson.noSpaces)
   }
 
-  wrapperClass.printAsJson()
+  def printEncode(user: User)(implicit userEncoder: Encoder[User]): Unit = {
+    println(user.asJson)
+  }
 
+  def printEncodeUsingWrapClass(user: User, employee: Employee)(
+      implicit userEncoder: Encoder[User],
+      employeeEncoder: Encoder[Employee]): Unit = {
+    new WrapperClass("hoge", "fuga", user, employee).printAsJson()
+  }
+
+  def printDecode(jsonString: String)(
+      implicit userDecoder: Decoder[User]): Unit = {
+    val decodedUser = decode[User](jsonString).right.get
+    println(decodedUser)
+  }
 }
